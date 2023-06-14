87129b8cb54c28f4d7b5ff52b38769a5e17edba7
LuisMeza35 <92602170+LuisMeza35@users.noreply.github.com>
2023-06-14 12:47:35 -0300

    segundo commit

--- Examen/target/classes/main.java
+++ Examen/target/classes/main.java
@@ -5,6 +5,7 @@ public class main {
 	private static final Logger LOG = Logger.getLogger(Main.class.getName());
 	
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 
 	}
